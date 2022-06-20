b43d3ddf81cb21c970915e52024a8f69c538cb17
kkuchta <kkuchta@griddynamics.com>
2022-06-20 17:28:00 +0200

    renamed main class

--- MessageParser/src/main/java/com/griddynamics/internship/MessageParser.java
+++ MessageParser/src/main/java/com/griddynamics/internship/MessageParser.java
@@ -1,6 +1,6 @@
 package com.griddynamics.internship;
 
-public class Main {
+public class MessageParser {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
