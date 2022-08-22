66698ccb6cecab07bcf244b7b89e50fd9712859e
kkuchta <kkuchta@griddynamics.com>
2022-08-22 12:38:09 +0200

    bytebuddy

--- Serializer/src/main/java/com/griddynamics/Main.java
+++ Serializer/src/main/java/com/griddynamics/Main.java
@@ -6,10 +6,15 @@ import com.griddynamics.dynamicproxy.base.TimingInvocationHandler;
 import com.griddynamics.serializer.base.pojo.Dog;
 import com.griddynamics.serializer.base.pojo.Person;
 import com.griddynamics.serializer.base.serializers.JSONSerializer;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.Map;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Main {
 
@@ -25,6 +30,21 @@ public class Main {
         final Person person = new Person(12, 2342, "name", new Dog("dogname", 2));
         final Printable printable = PrintableFactory.createPrintable(person, new JSONSerializer());
         printable.print();
+        //bytebuddy
+        try {
+            final Person instance = new ByteBuddy().subclass(Person.class)
+                                                  .method(named("toString"))
+                                                  .intercept(FixedValue.value("Hello world!"))
+                                                  .make()
+                                                  .load(ClassLoader.getSystemClassLoader())
+                                                  .getLoaded()
+                                                  .getDeclaredConstructor(int.class, int.class, String.class, Dog.class)
+                                                  .newInstance(12, 2312, "name", new Dog("dogname", 2));
+            System.out.println(instance);
+        }
+        catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            System.out.println("Something went wrong using bytebuddy");
+        }
     }
 
 }
