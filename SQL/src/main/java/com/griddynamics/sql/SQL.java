c81f81698dc0b28e7df24f1accb4d4cede678276
kkuchta <kkuchta@griddynamics.com>
2022-09-02 14:47:49 +0200

    initial commit

---
+++ SQL/src/main/java/com/griddynamics/sql/SQL.java
@@ -0,0 +1,40 @@
+package com.griddynamics.sql;
+
+import com.griddynamics.sql.base.User;
+import com.griddynamics.sql.service.UserService;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class SQL {
+
+    public static void main(String[] args) {
+        try {
+            final Properties properties = new Properties();
+            properties.setProperty("password", "password");
+            properties.setProperty("user", "user");
+            final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/database", properties);
+            final PreparedStatement preparedStatement = connection.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS users (id varchar UNIQUE NOT NULL, username varchar NOT NULL);");
+            preparedStatement.execute();
+            connection.prepareStatement("DELETE FROM users").execute();
+            final UserService userService = new UserService(connection);
+            for (int i = 0; i < 100; i++) {
+                userService.save(new User("" + i, "user" + i));
+            }
+            userService.delete("42");
+           // userService.delete("'1' OR 1=1");
+            userService.deleteThroughStatement("'1' OR 1=1");
+            final ResultSet result = connection.prepareStatement("SELECT * FROM users").executeQuery();
+            int count = 0;
+            while (result.next()) {
+                count++;
+            }
+            System.out.println(count);
+        }
+        catch (SQLException e) {
+            throw new IllegalStateException("Could not initialize the database!", e);
+        }
+    }
+
+}
---
+++ SQL/src/main/java/com/griddynamics/sql/base/User.java
@@ -0,0 +1,26 @@
+package com.griddynamics.sql.base;
+
+public class User {
+    private final String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    private final String username;
+
+    public User(final String id, final String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id='" + id + '\'' + ", username='" + username + '\'' + '}';
+    }
+
+}
